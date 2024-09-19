45f0cb830a5d0d8d094efa6d11288b505ed52eb7
Abdul Sami Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2024-09-19 15:42:12 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";          //treat all js code as newer version of javascript
+
+console.log(3 + 3);
+
+console.log("hitesh");
+
+let name ="Sami";
+
+let age = 18;
+
+let isLoggedIn = false;
+
+
+//number                 => range = 2 to power 53.
+//bigInt
+//String => ""
+//boolean => true/false
+//null => standAlone value
+//undefined => (e.g let name;)
+
+//symbol => unique;
+//object
+
+console.log(typeof "Sami");
+
+console.log(typeof undefined);
+console.log(typeof null);            //note this is object
