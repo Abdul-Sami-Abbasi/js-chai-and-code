f074e06ec029326e28676104b4bca420d3456191
Abdul Sami Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2024-09-21 09:20:20 +0000

    conversion of operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -40,8 +40,8 @@ valueInNumber = Number(score);
 let isLoggedIn = "";
 let booleanIsLoggedIn = Boolean(isLoggedIn); 
 
-console.log(typeof booleanIsLoggedIn); 
-console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn); 
+// console.log(booleanIsLoggedIn);
 
 //0 => false; 1 => true;
 //"" => false
@@ -49,5 +49,41 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = "";
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************************** Operations **************************************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello";
+let str2 = " Sami";
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+console.log(num1);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
