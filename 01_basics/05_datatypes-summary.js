68ca62424c6796c5cbab37fb21065b00589665de
Abdul Sami Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2024-09-30 10:58:03 +0000

    video number 09

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,36 @@
+/*
+    # Primitives (Call by Value)
+    7 types: Number, String, boolean, undefined, null, Symbol, BigInt 
+ */
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId); //false
+
+const bigNumber = 23256325478596563n;
+
+
+
+/* 
+    Non-Primitives (Call by Reference)
+    3 types: Array, Objects, Functions
+*/
+
+const heros = ["shaktiman", "nagraaj", "doga"];
+
+let myObj = {
+    name:"Sami",
+    age:22,
+}
+
+const myFuction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof id);
