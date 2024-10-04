1a69082f824792e2f45053d726c95926f463b625
Abdul Sami Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2024-10-04 11:35:43 +0000

    (video 16)

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+//singleton
+//object.create
+
+//object literals
+const jsUser = {
+    name: "Sami",
+    "full name": "Abdul Sami Abbasi",
+    age: 18,
+    location: "Karachi",
+    email: "sami@google.com",
+    isLoggedIn: false, 
+    lastLoginDays: ["Monday", "Saturday"],
+}
+
+console.log(jsUser["full name"]);
