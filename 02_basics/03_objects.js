3f855bbb2b442cf18390910e095d398d7298c671
Abdul Sami Abbasi <163078170+Abdul-Sami-Abbasi@users.noreply.github.com>
2024-10-05 11:59:28 +0000

    video 16 (objects part1)

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,15 +1,38 @@
-//singleton
-//object.create
+// singleton
+// Object.create
 
-//object literals
-const jsUser = {
-    name: "Sami",
-    "full name": "Abdul Sami Abbasi",
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
     age: 18,
-    location: "Karachi",
-    email: "sami@google.com",
-    isLoggedIn: false, 
-    lastLoginDays: ["Monday", "Saturday"],
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(jsUser["full name"]);
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
